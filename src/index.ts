f2f2b4273d196478203f1f8ec86b83dd1f7b70ec
root <root@localhost>
2023-07-02 22:47:35 +0200

    Simplified code

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-const isIterable = <T>(x: any): x is Iterable<T> => typeof x?.[Symbol.iterator] === 'function';
-const isAsyncIterable = <T>(x: any): x is AsyncIterable<T> => typeof x?.[Symbol.asyncIterator] === 'function';
+const isIterable = <T>(x: unknown extends Iterable<T> ? Iterable<T> : any): x is Iterable<T> => typeof x?.[Symbol.iterator] === 'function';
+const isAsyncIterable = <T>(x: unknown extends Iterable<T> ? Iterable<T> : any): x is AsyncIterable<T> => typeof x?.[Symbol.asyncIterator] === 'function';
+
+type MaybePromise<T> = T | Promise<T>;
 
 export namespace transform {
-	export type CallbackFn<T, R> = { (item: T): undefined | R | Iterable<R> | AsyncIterable<R> | Promise<R | Iterable<R> | AsyncIterable<R>> };
-	export type SyncCallbackFn<T, R> = { (item: T): undefined | R | Iterable<R> };
+	export type CallbackFn<T, R> = { (item: T): MaybePromise<R | Iterable<R> | AsyncIterable<R> | undefined> };
+	export type SyncCallbackFn<T, R> = { (item: T): R | Iterable<R> | undefined };
 }
 
-export async function* transform<T, R>(it: Iterable<T> | AsyncIterable<T>, fn: transform.CallbackFn<T, R>): AsyncGenerator<R> {
+export async function* transform<T, R>(it: Iterable<T> | AsyncIterable<T>, fn: transform.CallbackFn<T, R>) {
 	if (!isIterable(it) && !isAsyncIterable(it))
 		throw new Error('Argument type mismatch: The first argument is expected to be "iterable" or "asyncIterable".');
 
@@ -14,18 +16,16 @@ export async function* transform<T, R>(it: Iterable<T> | AsyncIterable<T>, fn: t
 		throw new Error(`Argument type mismatch: The second argument is expected to be a "function", but received "${typeof fn === 'object' ? (fn ? 'object' : 'null') : typeof fn}".`);
 
 	for await (const item of it) {
-		const trResult = await fn(item);
-		if (isIterable<R>(trResult) || isAsyncIterable<R>(trResult)) {
-			for await (const trItem of trResult) {
-				yield trItem;
-			}
-		} else if (typeof trResult !== 'undefined') {
-			yield trResult;
+		const result = await fn(item);
+		if (isIterable(result) || isAsyncIterable(result)) {
+			yield* result;
+		} else if (typeof result !== 'undefined') {
+			yield result;
 		}
 	}
 }
 
-export function* transformSync<T, R>(it: Iterable<T>, fn: transform.SyncCallbackFn<T, R>): Generator<R> {
+export function* transformSync<T, R>(it: Iterable<T>, fn: transform.SyncCallbackFn<T, R>) {
 	if (!isIterable(it))
 		throw new Error('Argument type mismatch: The first argument is expected to be "iterable".');
 
@@ -33,13 +33,11 @@ export function* transformSync<T, R>(it: Iterable<T>, fn: transform.SyncCallback
 		throw new Error(`Argument type mismatch: The second argument is expected to be a "function", but received "${typeof fn === 'object' ? (fn ? 'object' : 'null') : typeof fn}".`);
 
 	for (const item of it) {
-		const trResult = fn(item);
-		if (isIterable<R>(trResult)) {
-			for (const trItem of trResult) {
-				yield trItem;
-			}
-		} else if (typeof trResult !== 'undefined') {
-			yield trResult;
+		const result = fn(item);
+		if (isIterable(result)) {
+			yield* result;
+		} else if (typeof result !== 'undefined') {
+			yield result;
 		}
 	}
 }
